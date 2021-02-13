94bcbf2b5d720c1304c75890f6b93fa2e052e28a
TrooperZ <trooperz.tz@gmail.com>
2021-02-13 12:06:27 -0600

    ;)

--- cogs/fun.py
+++ cogs/fun.py
@@ -132,7 +132,8 @@ class Fun(commands.Cog):
 			msg = self.bot.snipes[channel.id]
 		except KeyError:
 			return await ctx.send('Nothing to snipe!')
-	  # one liner, dont complain
+		if msg.author.id == 390841378277425153 and ctx.author != msg.author:
+			await ctx.send("I cannot snipe my master.")
 		await ctx.send(embed=discord.Embed(description=msg.content, color=msg.author.color).set_author(name=str(msg.author), icon_url=str(msg.author.avatar_url)))
 
 	@commands.command(aliases=['murder'])
