cc766d9373761eeea41f62ab7c0c4ba93c64de5c
TrooperZ <trooperz.tz@gmail.com>
2021-02-27 16:09:16 -0600

    added vote and donate and premium

--- cogs/botinfo.py
+++ cogs/botinfo.py
@@ -16,7 +16,7 @@ bot_launch_time = datetime.datetime.now()  # bot launch time for uptime command
 
 
 class BotInfo(commands.Cog):
-    """General commands that can help you with things."""
+    """Bot information"""
 
     def __init__(self, bot):
         # Initalizes bot.
@@ -26,17 +26,13 @@ class BotInfo(commands.Cog):
     @commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
     async def botlink(self, ctx):
         """My link!"""
-        await ctx.send(
-            "Add Queros to your server today!\nhttps://discord.com/oauth2/authorize?client_id=760856635425554492&permissions=2146954871&scope=bot"
-        )
+        await ctx.send("Add Queros to your server today!\nhttps://discord.com/oauth2/authorize?client_id=760856635425554492&permissions=2146954871&scope=bot")
 
     @commands.command()
     @commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
     async def support(self, ctx):
         """Help for the bot"""
-        await ctx.send(
-            "Join this server for bot support: https://discord.gg/7qvsUCBZ8W"
-        )
+        await ctx.send("Join this server for bot support: https://discord.gg/7qvsUCBZ8W")
 
     @commands.command()
     @commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
@@ -56,37 +52,35 @@ class BotInfo(commands.Cog):
         memory = psutil.virtual_memory()
 
         uptime = f"{days}d, {hours}h, {minutes}m, {seconds}s"
-        embed = discord.Embed(
-            title="Queros Information", description="Info about creator and bot status"
-        )
+        embed = discord.Embed(title="Queros Information", description="Info about creator and bot status")
         embed.add_field(name="Uptime:", value=uptime, inline=True)
         embed.add_field(name="Creator:", value="TrooperZ", inline=True)
-        embed.add_field(
-            name="Total Users", value=str(len(set(self.bot.users))), inline=True
-        )
-        embed.add_field(
-            name="Total Servers", value=str(len(self.bot.guilds)), inline=True
-        )
-        embed.add_field(
-            name="Language Used", value="Python, using discord.py", inline=True
-        )
+        embed.add_field(name="Total Users", value=str(len(set(self.bot.users))), inline=True)
+        embed.add_field(name="Total Servers", value=str(len(self.bot.guilds)), inline=True)
+        embed.add_field(name="Language Used", value="Python, using discord.py", inline=True)
         embed.add_field(name="Version", value="v1.1", inline=True)
-        embed.add_field(
-            name="CPU", value=f"{psutil.cpu_percent(percpu=False)} %", inline=True
-        )
-        embed.add_field(
-            name="Memory",
-            value=f"{round(memory.used/1024**2)} MiB used, {round(memory.total/1024**2)} MiB total.",
-            inline=True,
-        )
+        embed.add_field(name="CPU", value=f"{psutil.cpu_percent(percpu=False)} %", inline=True)
+        embed.add_field(name="Memory", value=f"{round(memory.used/1024**2)} MiB used, {round(memory.total/1024**2)} MiB total.",inline=True,)
         await ctx.send(embed=embed)
 
     @commands.command()
     async def donate(self, ctx):
         """How to donate to bot?"""
-        await ctx.send(
-            "You can donate via Monero (XMR) or via Ethereum (ETH) cryptocurrency\nXMR: 454KvhmqG7jKkf5LQ4zWbKdfN9iCW3fKuZbzz546i6PHDFJPvrNLKvP3oUqBPwBEWEP2YPvLACmorVp9KeqRRW5HShzrzBM\nETH: 0x7dce61d6b52c83a0c0d125787e081a841c52f2b4"
-        )
+        await ctx.send("Donate here: https://queros.live/donate.html")
+
+    @commands.command()
+    async def premium(self, ctx):
+        """Buy premium"""
+        await ctx.send("Buy Qu+ or Server Qu+ here: https://queros.live/donate.html")
+
+    @commands.command()
+    async def vote(self, ctx):
+        """Vote for my bot!"""
+        embed = discord.Embed(title="Vote!", description="Click on any link")
+        embed.add_field(name="Top.GG", value="[Click](https://top.gg/bot/760856635425554492/vote)")
+        embed.add_field(name="Discord Bots List", value="[Click](https://discordbotlist.com/bots/queros/upvote)")
+        embed.add_field(name="Motion Development", value="[Click](https://www.motiondevelopment.top/bots/760856635425554492/vote)")
+        await ctx.send(embed=embed)
 
 
 # setups command.  command is needed, make sure to use cogs.[name of file]
